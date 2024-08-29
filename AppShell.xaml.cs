1798f438d04d282ca21a3f7cc7c29ad803c2a2cc
Snowkai <34677392+Snowkai@users.noreply.github.com>
2024-08-29 15:19:43 +0300

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PCAssembly
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Resources/src/pcinterfaces/Ram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCAssembly.Resources.src.pcinterfaces
+{
+    internal class Ram
+    {
+        private string[] RamType = {"DDR1", "DDR2", "DDR3", "DDR4", "DDR5"};
+    }
+}
