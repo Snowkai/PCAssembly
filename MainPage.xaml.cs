a3ee9abba82739557ddaf4ed0e57caf3ca01dcb4
Snowkai <34677392+Snowkai@users.noreply.github.com>
2024-09-26 22:23:55 +0300

    Add CollectionView
     CollectionView work and database data worked too. Need DataTemplate change on button and thinking about view info.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,44 @@
-﻿namespace PCAssembly
+﻿using Microsoft.Maui.Controls;
+using PCAssembly.src.db;
+using PCAssembly.src.db.Models;
+using System.Collections.ObjectModel;
+
+namespace PCAssembly
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();   
+            UpdateColView();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var db = new Database();
+            // Обновите данные здесь
+            colView.ItemsSource = await db.GetItemsAsync();
         }
 
         private void OnButtonCliked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new Pages.PCConfig());
+        }
+        private async void UpdateColView()
+        {
+            var db = new Database();
+            ObservableCollection<PC> pcList = new ObservableCollection<PC>(await db.GetItemsAsync());
+            colView.ItemsSource = pcList;
+            colView.ItemTemplate = new DataTemplate(() =>
+            {
+                var configNameLabel = new Label();
+                configNameLabel.SetBinding(Label.TextProperty, "Name");
+
+                return new StackLayout {
+                Children = {configNameLabel} 
+                };
+            });
         }
     }
 }
